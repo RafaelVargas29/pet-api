4f267687ad1581f96fe7bc9908ff700501eef57c
RafaelVargas29 <79914727+RafaelVargas29@users.noreply.github.com>
2022-11-21 13:11:05 -0300

    Retrieving more data about breeds

--- resources/views/pets/breeds.blade.php
+++ resources/views/pets/breeds.blade.php
@@ -7,8 +7,12 @@
     
     @foreach($data_breed as $d)
             
-        <p>Nome: {{ $d->name }}</p>
-        <p>Vida útil: {{ $d->life_span }}</p><br>
+        <p>Nome: {{ $d->name ?? ''}}</p>
+        <p>Raça para procriar: {{ $d->bred_for ?? ''}}</p>
+        <p>Grupo de raça: {{ $d->breed_group ?? '' }}</p>
+        <p>Vida útil: {{ $d->life_span  ?? ''}}</p>
+        <p>Temperamento: {{ $d->temperament ?? '' }}</p>
+        <p>Origem: {{ $d->origin ?? '' }}</p><br><br>
 
     @endforeach
 
